5157618613a15bce1b701f30c5087e2e01d2d81d
kobancheg <aleks.revati@gmail.com>
2021-09-14 23:26:28 +0400

    fix docker-compose

--- src/database/seeders/20210910154434-demo-user.js
+++ src/database/seeders/20210910154434-demo-user.js
@@ -6,7 +6,7 @@ module.exports = {
             {
                 user_name: 'mlarter0',
                 email: 'bwrench0@timesonline.co.uk',
-                telephone: '+7-8634 444 55 10',
+                telephone: '6344445510',
                 is_active: false,
                 password: 'orQtNV6Aque',
                 createdAt: '2020-11-02 14:48:47',
@@ -15,7 +15,7 @@ module.exports = {
             {
                 user_name: 'ibinstead1',
                 email: 'mskirven1@guardian.co.uk',
-                telephone: '+7-8634 444 55 10',
+                telephone: '6344445510',
                 is_active: false,
                 password: 'EqwNJNrlf6',
                 createdAt: '2020-11-02 14:48:47',
@@ -24,7 +24,7 @@ module.exports = {
             {
                 user_name: 'hreace2',
                 email: 'lchisholme2@soundcloud.com',
-                telephone: '+7-8634 444 55 10',
+                telephone: '6344445510',
                 is_active: true,
                 password: 'vaoi9t3wN6sV',
                 createdAt: '2020-11-02 14:48:47',
@@ -33,7 +33,7 @@ module.exports = {
             {
                 user_name: 'rhannan3',
                 email: 'ldrewery3@linkedin.com',
-                telephone: '+7-8634 444 55 10',
+                telephone: '6344445510',
                 is_active: false,
                 password: '4YZUSIHOa0',
                 createdAt: '2020-11-02 14:48:47',
@@ -42,7 +42,7 @@ module.exports = {
             {
                 user_name: 'jkneel4',
                 email: 'hgetsham4@cdc.gov',
-                telephone: '+7-8634 444 55 10',
+                telephone: '6344445510',
                 is_active: true,
                 password: 'vlp7gLAxI',
                 createdAt: '2020-11-02 14:48:47',
@@ -51,7 +51,7 @@ module.exports = {
             {
                 user_name: 'gpoulett5',
                 email: 'ldorrins5@godaddy.com',
-                telephone: '+7-8634 444 55 10',
+                telephone: '6344445510',
                 is_active: true,
                 password: 'ii1paH',
                 createdAt: '2020-11-02 14:48:47',
@@ -60,7 +60,7 @@ module.exports = {
             {
                 user_name: 'drobley6',
                 email: 'rvuittet6@house.gov',
-                telephone: '+7-8634 444 55 10',
+                telephone: '6344445510',
                 is_active: true,
                 password: 'ZP0Fpx',
                 createdAt: '2020-11-02 14:48:47',
@@ -69,7 +69,7 @@ module.exports = {
             {
                 user_name: 'avennings7',
                 email: 'ethompson7@woothemes.com',
-                telephone: '+7-8634 444 55 10',
+                telephone: '6344445510',
                 is_active: true,
                 password: 'WClhL4X',
                 createdAt: '2020-11-02 14:48:47',
@@ -78,7 +78,7 @@ module.exports = {
             {
                 user_name: 'ynoriega8',
                 email: 'bpovele8@disqus.com',
-                telephone: '+7-8634 444 55 10',
+                telephone: '6344445510',
                 is_active: false,
                 password: 'lhK4HGElll',
                 createdAt: '2020-11-02 14:48:47',
@@ -87,7 +87,7 @@ module.exports = {
             {
                 user_name: 'laloshechkin9',
                 email: 'lrodd9@nytimes.com',
-                telephone: '+7-8634 444 55 10',
+                telephone: '6344445510',
                 is_active: true,
                 password: 'pmRDTW4Sw9Tn',
                 createdAt: '2020-11-02 14:48:47',
@@ -96,7 +96,7 @@ module.exports = {
             {
                 user_name: 'rvlacha',
                 email: 'bmacsharrya@devhub.com',
-                telephone: '+7-8634 444 55 10',
+                telephone: '6344445510',
                 is_active: true,
                 password: 'RdIODxon',
                 createdAt: '2020-11-02 14:48:47',
@@ -105,7 +105,7 @@ module.exports = {
             {
                 user_name: 'erichardsonb',
                 email: 'wgandersb@dropbox.com',
-                telephone: '+7-8634 444 55 10',
+                telephone: '6344445510',
                 is_active: false,
                 password: 'oYozFDkLW',
                 createdAt: '2020-11-02 14:48:47',
@@ -114,7 +114,7 @@ module.exports = {
             {
                 user_name: 'bmcimmiec',
                 email: 'randrickc@cargocollective.com',
-                telephone: '+7-8634 444 55 10',
+                telephone: '6344445510',
                 is_active: true,
                 password: 'ADcITqBCf2Rf',
                 createdAt: '2020-11-02 14:48:47',
@@ -123,7 +123,7 @@ module.exports = {
             {
                 user_name: 'zbeeresd',
                 email: 'scorwind@mtv.com',
-                telephone: '+7-8634 444 55 10',
+                telephone: '6344445510',
                 is_active: true,
                 password: '3MSKoBq6z',
                 createdAt: '2020-11-02 14:48:47',
@@ -132,7 +132,7 @@ module.exports = {
             {
                 user_name: 'biannelloe',
                 email: 'skiltiee@eepurl.com',
-                telephone: '+7-8634 444 55 10',
+                telephone: '6344445510',
                 is_active: false,
                 password: 'VIa3Ks4',
                 createdAt: '2020-11-02 14:48:47',
@@ -141,7 +141,7 @@ module.exports = {
             {
                 user_name: 'gskalaf',
                 email: 'gcowderyf@toplist.cz',
-                telephone: '+7-8634 444 55 10',
+                telephone: '6344445510',
                 is_active: true,
                 password: 'mDhWCuiLV1n',
                 createdAt: '2020-11-02 14:48:47',
@@ -150,7 +150,7 @@ module.exports = {
             {
                 user_name: 'obrosolig',
                 email: 'kottleyg@imdb.com',
-                telephone: '+7-8634 444 55 10',
+                telephone: '6344445510',
                 is_active: true,
                 password: 'gFgS2Qm',
                 createdAt: '2020-11-02 14:48:47',
@@ -159,7 +159,7 @@ module.exports = {
             {
                 user_name: 'mskeah',
                 email: 'llancetth@wikispaces.com',
-                telephone: '+7-8634 444 55 10',
+                telephone: '6344445510',
                 is_active: false,
                 password: 'a95fZnI',
                 createdAt: '2020-11-02 14:48:47',
@@ -168,7 +168,7 @@ module.exports = {
             {
                 user_name: 'tkinsetti',
                 email: 'credmillei@edublogs.org',
-                telephone: '+7-8634 444 55 10',
+                telephone: '6344445510',
                 is_active: false,
                 password: 'xCnYk0Uw1',
                 createdAt: '2020-11-02 14:48:47',
@@ -177,7 +177,7 @@ module.exports = {
             {
                 user_name: 'cvizorj',
                 email: 'bhampsteadj@ning.com',
-                telephone: '+7-8634 444 55 10',
+                telephone: '6344445510',
                 is_active: true,
                 password: 'N2eMWtm3k',
                 createdAt: '2020-11-02 14:48:47',
